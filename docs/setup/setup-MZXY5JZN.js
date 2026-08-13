e00318a879fec5c2e7eed08470d83ae0b7988454
aurelain <aurelain@gmail.com>
2026-08-13 07:50:49 +0300

    implemented hashed building

---
+++ docs/setup/setup-MZXY5JZN.js
@@ -0,0 +1,1 @@
+(()=>{async function n(){let e=document.getElementById("setup");e.innerHTML="hello"}n();})();
--- scripts/build/build.js
+++ scripts/build/build.js
@@ -1,11 +1,14 @@
 import fs from 'node:fs';
+import path from 'node:path';
 import process from 'node:process';
 import esbuild from 'esbuild';
 
 // =====================================================================================================================
 //  D E C L A R A T I O N S
 // =====================================================================================================================
-const isDev = process.argv.includes('--dev');
+const DESTINATION_DIR = 'docs';
+const args = Array.from(process.argv);
+const isDev = args.includes('--dev');
 
 // =====================================================================================================================
 //  P U B L I C
@@ -13,7 +16,13 @@ const isDev = process.argv.includes('--dev');
 /**
  *
  */
-const build = async () => {};
+async function build() {
+    const {target, destinationDir} = decidePaths();
+
+    removeOldJsFileFrom(destinationDir);
+    const {filePath} = await createBundle(target, destinationDir);
+    updateIndexHtml(destinationDir, filePath);
+}
 
 // =====================================================================================================================
 //  P R I V A T E
@@ -21,7 +30,68 @@ const build = async () => {};
 /**
  *
  */
-const createBundle = async (target, outputPattern) => {};
+function decidePaths() {
+    const target = args.find((arg) => arg.startsWith('src'));
+    const parentDirName = target.split('/').at(-2).toString();
+    const destinationDir = path.join(DESTINATION_DIR, parentDirName);
+    return {target, destinationDir};
+}
+
+/**
+ *
+ */
+function removeOldJsFileFrom(dirPath) {
+    const jsFilePath = findJsFile(dirPath);
+    if (jsFilePath) {
+        fs.unlinkSync(jsFilePath);
+        const mapFilePath = jsFilePath + '.map';
+        if (fs.existsSync(mapFilePath)) {
+            fs.unlinkSync(mapFilePath);
+        }
+    }
+}
+
+/**
+ *
+ */
+function findJsFile(dirPath) {
+    for (const file of fs.readdirSync(dirPath)) {
+        if (file.endsWith('js')) {
+            return path.join(dirPath, file);
+        }
+    }
+}
+
+/**
+ *
+ */
+async function createBundle(target, outdir) {
+    await esbuild.build({
+        entryPoints: [target],
+        bundle: true,
+        minify: !isDev,
+        sourcemap: isDev,
+        outdir,
+        entryNames: '[name]-[hash]',
+    });
+
+    const filePath = findJsFile(outdir);
+    return {
+        filePath,
+        content: fs.readFileSync(filePath, 'utf8'),
+    };
+}
+
+/**
+ *
+ */
+function updateIndexHtml(dirPath, jsFilePath) {
+    const indexPath = path.join(dirPath, 'index.html');
+    const indexContent = fs.readFileSync(indexPath, 'utf8');
+    const jsFileName = jsFilePath.split(path.sep).pop().toString();
+    const freshContent = indexContent.replace(/(['"]).*?js(['"])/, `$1${jsFileName}$2`);
+    fs.writeFileSync(indexPath, freshContent, 'utf8');
+}
 
 // =====================================================================================================================
 //  R U N
