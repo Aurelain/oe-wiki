7e9adaf720f2e8f60455de0051a3282479a3e844
aurelain <aurelain@gmail.com>
2026-09-09 13:07:29 +0300

    styled the container

---
+++ src/planner/CSS.js
@@ -0,0 +1,22 @@
+import styled from './utils/styled.js';
+
+// noinspection CssUnusedSymbol
+const CSS = styled`
+    .planner {
+        //text-align: center;
+    }
+
+    .planner .content {
+        container-type: inline-size;
+        width: 100%;
+        margin: auto;
+        max-width: 708px;
+    }
+
+    .planner .bg {
+        width: 100%;
+        filter: drop-shadow(0 0 0.3rem black);
+    }
+`;
+
+export default CSS;
---
+++ src/planner/HTML.js
@@ -0,0 +1,10 @@
+import CSS from './CSS.js';
+
+const HTML = `
+<style>${CSS}</style>
+<div class='content'>
+    <img class='bg' src='@bgUrl' alt='empty background'/>
+</div>  
+`;
+
+export default HTML;
