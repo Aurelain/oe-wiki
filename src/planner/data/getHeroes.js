66573a3b8aea9ac3df02ab17a249a89385771d5c
aurelain <aurelain@gmail.com>
2026-09-11 11:44:14 +0300

    finished collecting the hero list

---
+++ src/planner/data/getHeroes.js
@@ -0,0 +1,130 @@
+import match from '../utils/match.js';
+import downloadPage from '../helpers/downloadPage.js';
+
+// =====================================================================================================================
+//  P U B L I C
+// =====================================================================================================================
+/**
+ *
+ */
+async function getHeroes() {
+    const overview = await downloadPage('Heroes_Overview');
+    const [table = ''] = match(overview, /<table[\s\S]*?<\/table>/);
+    const rows = table.split('</tr>');
+    rows.shift(); // remove the header
+
+    const heroes = {};
+    for (let i = 0; i < rows.length; i++) {
+        const hero = parseRow(rows[i], rows[i + 1]);
+        if (hero) {
+            heroes[hero.id] = hero;
+        }
+    }
+    return heroes;
+}
+
+// =====================================================================================================================
+//  P R I V A T E
+// =====================================================================================================================
+/**
+ *
+ */
+function parseRow(row, extraRow) {
+    const [iconColumn, nameColumn, , classColumn, specIconColumn, specInfoColumn, skillsColumn] = row.split('</td>');
+
+    // id
+    let [, id] = match(iconColumn, /data-id=['"]([^'"]*)/);
+    if (!id) {
+        return;
+    }
+    id = id.replaceAll('&#95;', '_');
+
+    // icon
+    const [, icon] = match(iconColumn, /src=['"]([^'"]*)/);
+    if (!icon) {
+        return console.warn('No icon!');
+    }
+
+    // portrait
+    const portrait = icon.replace(/\d*px/, '600px'); // dangerous
+
+    // name
+    let [, name] = match(nameColumn, /<a.*?>([^<]*)/);
+    if (!name) {
+        return console.warn('No name!');
+    }
+    // console.log('name:', name);
+
+    // classIcon
+    const [, classIcon] = match(classColumn, /src=['"]([^'"]*)/);
+    if (!classIcon) {
+        return console.warn('No classIcon!');
+    }
+
+    // className
+    const [, className] = match(classColumn, /<a.*?>([^<]+)/);
+    if (!className) {
+        return console.warn('No className!');
+    }
+
+    // specIcon
+    const [, specIcon] = match(specIconColumn, /src=['"]([^'"]*)/);
+    if (!specIcon) {
+        return console.warn('No specIcon!');
+    }
+
+    // specName
+    const [, specName] = match(specInfoColumn, /<b>([^<]+)/);
+    if (!specName) {
+        return console.warn('No specName!');
+    }
+
+    // specDescription
+    const [, specDescription] = match(extraRow || '', /<td.*?>([^<]+)/);
+    if (!specDescription) {
+        return console.warn('No specDescription!');
+    }
+
+    // skills
+    const skills = parseSkills(skillsColumn);
+    if (!skills) {
+        return;
+    }
+
+    return {id, icon, portrait, classIcon, className, specIcon, specName, specDescription, skills};
+}
+
+/**
+ *
+ */
+function parseSkills(column) {
+    const divs = column.split('</div>');
+    const skills = [];
+    for (const div of divs) {
+        if (!div) {
+            continue;
+        }
+        const [, src] = match(div, /src=['"]([^'"]*)/);
+        if (!src) {
+            return console.warn('No skill src!');
+        }
+        let [, name] = match(src, /\d+px-(.*?)\.\w+$/);
+        name = name.toLowerCase();
+        let [, level = 1] = match(name, /_(\d+)$/);
+        if (level !== 1) {
+            name = name.replace(/_\d+$/, '');
+            level = Number(level);
+        }
+        if (name.includes('faction')) {
+            skills.unshift({name, level});
+        } else {
+            skills.push({name, level});
+        }
+    }
+    return skills;
+}
+
+// =====================================================================================================================
+//  E X P O R T
+// =====================================================================================================================
+export default getHeroes;
--- src/planner/planner.js
+++ src/planner/planner.js
@@ -1,11 +1,6 @@
-import match from './utils/match.js';
 import mount from './helpers/mount.jsx';
 import App from './components/App.jsx';
-
-// =====================================================================================================================
-//  D E C L A R A T I O N S
-// =====================================================================================================================
-const wikiUrl = getWikiUrl();
+import getHeroes from './data/getHeroes.js';
 
 // =====================================================================================================================
 //  P U B L I C
@@ -25,63 +20,6 @@ async function planner() {
         levelUrl: root.querySelector('.level img').src,
         heroes: await getHeroes(),
     });
-}
-
-// =====================================================================================================================
-//  P R I V A T E
-// =====================================================================================================================
-/**
- *
- */
-function getWikiUrl() {
-    let {href} = window.location;
-    href = href.replace(/\/..$/, ''); // remove normal language suffix (e.g. `/fr`)
-    href = href.replace(/\/..-\w+$/, ''); // remove advanced language suffix (e.g. `/zh-hans`)
-    href = href.replace(/[^/]*$/, ''); // remove advanced language suffix (e.g. `/zh-hans`)
-    return href;
-}
-
-/**
- *
- */
-async function downloadPage(page) {
-    let response;
-    try {
-        response = await fetch(wikiUrl + page);
-    } catch (e) {
-        return '';
-    }
-    return await response.text();
-}
-
-/**
- *
- */
-async function getHeroes() {
-    const overview = await downloadPage('Heroes_Overview');
-    const rows = match(overview, /<tr class=['"]hero['"][\s\S]*?<\/tr>/g);
-    const heroes = {};
-    for (const [row] of rows) {
-        let [, id] = match(row, /data-id=['"]([^'"]*)/);
-        if (!id) {
-            console.warn('No id!');
-            continue;
-        }
-        id = id.replaceAll('&#95;', '_');
-        const [, icon] = match(row, /src=['"]([^'"]*)/);
-        if (!icon) {
-            console.warn('No icon!');
-            continue;
-        }
-        let [, portrait] = match(row, /srcset=['"].*?([^ ]*) 2x/);
-        if (!portrait) {
-            console.warn('No portrait!');
-            continue;
-        }
-        portrait = portrait.replaceAll('140px', '600px');
-        heroes[id] = {id, icon, portrait};
-    }
-    return heroes;
 }
 
 // =====================================================================================================================
